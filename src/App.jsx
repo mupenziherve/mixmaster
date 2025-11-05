15b2e088dca9f0bdeaf4dc60dab891149c87beab
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-05 10:38:43 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h2>Home Page</h2>,
+  },
+  {
+    path: '/about',
+    element: <div>
+      <h2>About page</h2>
+    </div>,
+  },
+]);
+
 const App = () => {
-  return <h2>mixmaster starter</h2>;
-};
-export default App;
+  return <h1>Mixmaster starter</h1>
+}
+
+export default App
