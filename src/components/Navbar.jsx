4834600e2bf6cc41e8335bacd09aec0cb8b62137
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-06 22:39:12 +0200

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
-
+import styled from "styled-components";
 
 const Navbar = () => {
   return (
     <nav>
       <div className="nav-center">
+        <StyledBtn>Styled btn</StyledBtn>
         <span className="logo">MixMaster</span>
         <div className="nav-links">
           <NavLink to="/" className="nav-link">
