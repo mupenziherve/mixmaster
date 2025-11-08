88552c87dd8bd6a2c778e7e5eb10c066c90f6bc3
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-08 23:35:41 +0200

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,9 +5,6 @@ const About = () => {
     <div>
       <h1>About</h1>
       <Outlet/>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque facilis libero voluptas, excepturi enim autem? Est maxime iusto quo aperiam incidunt. Saepe vel cupiditate rem atque a, magnam corrupti labore!
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias maxime commodi veniam soluta enim molestiae exercitationem dolorem eum atque suscipit. Provident earum ullam enim aut ut iste nostrum placeat voluptates.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et eos quae beatae optio, voluptas a perferendis assumenda eius velit exercitationem voluptates omnis aut molestiae atque corrupti nisi, tenetur unde nesciunt!
     </div>
   )
   
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -6,8 +6,6 @@ const HomeLayout = () => {
 
     <Navbar/>
     <Outlet/>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe praesentium sunt ea hic deleniti debitis iusto nostrum, amet, nesciunt tenetur atque, odio laborum quisquam dolores error laboriosam veniam voluptas? Veritatis.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium minus tempora nulla unde! Velit saepe quidem culpa. Facere magni dicta beatae in nostrum, optio iure est, cum accusantium deleniti incidunt!
 
   </> ;
 };
