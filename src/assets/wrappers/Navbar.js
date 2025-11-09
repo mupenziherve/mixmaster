c484372665a2deb845cdfeb677cc741882228f93
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-09 12:49:42 +0200

    update

--- src/assets/wrappers/Navbar.js
+++ src/assets/wrappers/Navbar.js
@@ -1,7 +1,45 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.nav`
- 
-`;
+  background: var(--white);
+  .nav-center {
+    width: var(--view-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    padding: 1.5rem 2rem;
+  }
+
+  .logo {
+    font-size: clamp(1.5rem, 3vw, 3rem);
+    color: var(--primary-500);
+    font-weight: 700;
+    letter-spacing: 2px;
+  }
+
+  .nav-links {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    margin-top: 1rem;
+  }
+
+  .nav-link {
+    color: var(--grey-900);
+    padding: 0.5rem 0.5rem 0.5rem 0;
+    transition: var(--transition);
+    letter-spacing: 2px;
+  }
+  .nav-link {
+    color: var(--primary-500);
+  }
+  /* active */
+  @media (min-width: 768px) {
+    
+  }
+
+
+`
 
 export default Wrapper;
